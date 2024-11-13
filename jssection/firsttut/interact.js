a7d2d20d940411cf10213575d3c73e6487d9b948
Shudapp <dragosandrei9321@gmail.com>
2024-11-13 21:08:34 +0200

    Made a Rock-Papper-Scissors Game

---
+++ jssection/firsttut/interact.js
@@ -0,0 +1,78 @@
+
+
+
+function GetRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+function GetComputerChoice(){
+    return GetRandomInt(3)
+}
+function GetHumanChoice(){
+    let HumanAnswer = prompt("Choose Between R,P,S");
+    let HumanInt = 0;
+    if(HumanAnswer === "Rock"){
+        HumanInt = 0;
+    } else if(HumanAnswer=== "Paper"){
+        HumanInt = 1;
+    } else if(HumanAnswer === "Scissors"){
+        HumanInt = 2;
+    }
+    return HumanInt;
+}
+let HumanScore = 0, ComputerScore = 0;
+
+function PlayRound(HumanAnswer, ComputerAnswer){
+    if(HumanAnswer === ComputerAnswer){
+        return 0; // draw
+    }
+    if(HumanAnswer === '0'){
+        if(ComputerAnswer === '1'){
+            return -1; //ComputerWins
+        } else if(ComputerAnswer ==='2'){
+            return 1; //Human Wins
+        }
+    } else if(HumanAnswer == '1'){ // Om Hartie
+        if(ComputerAnswer === '2'){ // Computer Foarfeca
+            return -1; // Computer Wins
+        } else if(ComputerAnswer == '0'){ //Computer Piatra
+            return 1; // Human Wins
+        }
+    } else if(HumanAnswer == '2'){
+        if(ComputerAnswer == '0') {
+            return -1 // Computer Wins
+        } else if(ComputerAnswer =='1'){
+            return 1; // Human Wins;
+        }
+    }
+}
+function playGame(){
+    let HumanScore = 0, ComputerScore = 0;
+    for (let index = 1; index <= 5; index++) {
+        let HumanAnswer = GetHumanChoice();
+        let ComputerAnswer = GetComputerChoice();
+        let winner = PlayRound(HumanAnswer, ComputerAnswer);
+        console.log(winner);
+        if(winner === 0){
+            HumanScore++;
+            ComputerScore++;
+            console.log("You tied");
+        } else if(winner === 1){
+            console.log("Won One round");
+            HumanScore++;
+        } else if(winner === -1){
+            console.log("Lost One Round");
+            ComputerScore++;
+        }
+    }
+    if(HumanScore === ComputerScore){
+        console.log("DRAW\n");
+    } else if(HumanScore > ComputerScore){
+        console.log("You won\n");
+    } else if(HumanScore < ComputerScore){
+        console.log("You lost\n");
+    }
+    
+    
+}
+playGame();
+
